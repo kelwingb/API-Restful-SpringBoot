f3c13e23638c13d2a5137aa6e124e8bef1bfbef9
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-08-19 08:02:11 -0300

    Create UserService

---
+++ src/main/java/com/kelwingb/myproject/services/UserService.java
@@ -0,0 +1,54 @@
+package com.kelwingb.myproject.services;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kelwingb.myproject.models.User;
+import com.kelwingb.myproject.repositories.TaskRepository;
+import com.kelwingb.myproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+  @Autowired
+  private UserRepository userRepository;
+
+  private TaskRepository taskRepository;
+  
+  public User findById(Long id) {
+    Optional<User> user = this.userRepository.findById(id);
+    if (user.isPresent()) {
+      return user.get();
+    } else {
+    return user.orElseThrow(() -> new RuntimeException(
+      "Usuário não encontrado! id: " + id + ", tipo: " + User.class.getName()
+    ));
+    }
+  }
+    
+    @Transactional
+    public User create(User obj) {
+      obj.setId(null);
+      obj = this.userRepository.save(obj);
+      this.taskRepository.saveAll(obj.getTasks());
+      return obj;
+    }
+    @Transactional
+    public User update (User obj) {
+      User newObj = findById(obj.getId());
+      newObj.setPassword(obj.getPassword());
+      return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+      findById(id);
+      try {
+        this.userRepository.deleteById(id);
+      } catch (Exception e) {
+        throw new RuntimeException("Não é possível excluir o usuário com id: " + id + ", pois ele possui tarefas associadas.");
+      }
+    }
+}
